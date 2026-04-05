cfef92193eaf69861244c7c8ea20a1262a0ef0b7
hackdumping <erictchoupe5@gmail.com>
2026-04-06 00:09:01 +0100

    Amelioration Bugs

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -5,6 +5,8 @@ echo "BUILD START"
 # Install dependencies
 python3.12 -m pip install -r requirements.txt
 
+python3.12 manage.py migrate
+
 # Run collectstatic
 python3.12 manage.py collectstatic --noinput --clear
 
