547f10e842a596f1855aa4ebd142cb4249ccacbb
hackdumping <erictchoupe5@gmail.com>
2026-04-06 01:15:09 +0100

    Amelioration Bugs

--- backend/vura_core/urls.py
+++ backend/vura_core/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+from django.core.management import call_command
+
+def run_migrations(request):
+    try:
+        call_command('migrate', interactive=False)
+        return HttpResponse("Migrations successfully executed!")
+    except Exception as e:
+        return HttpResponse(f"Error during migrations: {str(e)}", status=500)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/migrate/', run_migrations), # URL temporaire pour migrer la DB
     path('api/users/', include('users.urls')),
     path('api/forms/', include('forms.urls')),
     path('api/integrations/', include('integrations.urls')),
